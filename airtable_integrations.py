2da8525efed68b5560d5236020690cfefa6f32b1
Martin <ryqiem@gmail.com>
2018-11-28 13:19:16 +0100

    Refactored airtable_integrations

--- airtable_integrations.py
+++ airtable_integrations.py
@@ -2,14 +2,14 @@ from airtable import Airtable
 from credentials import AIRTABLE_API_KEY
 from pprint import pprint
 
-def make_dictionary_of_base(baseKey="app2VbjON28CVSlD1", table="Tasks", view='Grid view', sort="Name"):
+def make_dictionary_of_base(baseKey="app2VbjON28CVSlD1", table="Tasks", view='Grid view', key_column="Name", value_column="ID"):
 
     airtable = Airtable(baseKey, table, api_key=AIRTABLE_API_KEY)
 
-    task_name_project_id = {}
+    dict = {}
 
-    for page in airtable.get_iter(view=view, sort=sort):
+    for page in airtable.get_iter(view=view, sort=key_column):
         for record in page:
-            task_name_project_id[record['fields']['Name']] = record['fields']['ID'][0]
+            dict[record['fields'][key_column]] = record['fields'][value_column]
 
-    return task_name_project_id
+    return dict
